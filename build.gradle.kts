40e41f8664b5a5f4146c4ceda974b7bc42e6b114
rodolfovales <156723634+rodolfovales@users.noreply.github.com>
2026-08-26 18:14:31 -0300

    build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+}
+
+tasks.register("clean").configure {
+    delete(rootProject.layout.buildDirectory)
 }
 
 android {

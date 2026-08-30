1a298303ee9447f6aeb335cfc0169089bfb80fc4
rodolfovales <156723634+rodolfovales@users.noreply.github.com>
2026-08-30 19:37:10 -0300

    build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    id 'com.google.gms.google-services'
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }

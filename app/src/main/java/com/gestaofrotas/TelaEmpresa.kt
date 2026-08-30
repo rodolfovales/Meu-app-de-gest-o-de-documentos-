0e2cee750244df2cbc2dec8e698de56bdee0ca51
rodolfovales <156723634+rodolfovales@users.noreply.github.com>
2026-08-30 12:42:42 -0300

    TelaEmpresa.kt

--- app/src/main/java/com/gestaofrotas/TelaEmpresa.kt
+++ app/src/main/java/com/gestaofrotas/TelaEmpresa.kt
@@ -1,16 +1,17 @@
 package com.gestaofrotas
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.tela_empresa.*
 
 class TelaEmpresa : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tela_empresa)
 
-        botao_cadastrar.setOnClickListener {
+        val botaoCadastrar: Button = findViewById(R.id.botao_cadastrar)
+        botaoCadastrar.setOnClickListener {
             Toast.makeText(this, "Cadastro enviado com sucesso!", Toast.LENGTH_LONG).show()
             finish()
         }
